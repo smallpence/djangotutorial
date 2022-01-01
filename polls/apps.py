4bf0b151533d70deec6f618d4d81d146ed99e713
smallpence <smallpence.uk@gmail.com>
2022-01-01 11:52:52 +0000

    added question & choice models to the program

--- polls/apps.py
+++ polls/apps.py
@@ -1,9 +1,9 @@
-"not sure as of now what this does"
+"app metadata - used for importing"
 
 from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
-    "nor this"
+    "config for polls"
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,28 @@
 "modelling for polls app"
+import datetime
 
-# from django.db import models
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Question(models.Model):
+    "model for a question, with some text & date of publication"
+    text = models.CharField(max_length=200, verbose_name='question text')
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return str(self.text)
+
+    def was_published_recently(self):
+        "returns true if this question was published today"
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    "model for a choice for a question, and counts some amount of votes for it"
+    question = models.ForeignKey(Question, models.CASCADE)
+    text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.text)
