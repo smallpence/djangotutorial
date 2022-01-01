338bd1c7c2caf4aa1bb3df083a78fbddb05a556b
smallpence <smallpence.uk@gmail.com>
2022-01-01 11:57:30 +0000

    added admin site & question to it

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,8 @@
 "admin layout for polls"
 
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+
+admin.site.register(Question)
