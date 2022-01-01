6f710e7660ff6494a699d2de2a37127abe27b598
smallpence <smallpence.uk@gmail.com>
2022-01-01 15:39:48 +0000

    made some more urls with more complex paths

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,22 @@
 
 from django.http import HttpResponse
 
+
 def index(_):
-    "/"
+    "root to ensure no root 404"
     return HttpResponse("Hello world from polls!")
+
+
+def detail(_, question_id):
+    "just print some information about a question"
+    return HttpResponse(f"question id {question_id}")
+
+
+def result(_, question_id):
+    "show the current results for a question"
+    return HttpResponse(f"question results for {question_id}")
+
+
+def vote(_, question_id):
+    "allow user to vote for a question"
+    return HttpResponse(f"question voting for {question_id}")
